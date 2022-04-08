3ec9600e0a14f78efa816b4fc182fd0c44cf2ee8
DOUGLAS PINTO DO NASCIMENTO <shrek18.5@gmail.com>
2022-04-08 19:59:41 -0300

    Add files via upload

---
+++ Exercicio_4.py
@@ -0,0 +1,14 @@
+# Desenvolva um programa que lê dois valores numéricos inteiros e compara se o primeiro é maior que o segundo,
+# que o primeiro valor digitado é maior que o segundo.
+# Utilizando uma condicional simples. Caso seja (resultado verdadeiro), ele imprime na tela a mensagem informando
+
+# Lê dois valores interiors e compara ambos.
+
+x = int(input('Digite um valor inteiro: '))
+
+y = int(input('Digite um segundo valor inteiro: '))
+
+if(x > y):
+    print('O primeiro valor é maior que o segundo!')
+else:
+    print('O Segundo valor é maior que o primeiro')
