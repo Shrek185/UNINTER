8555bb797a6da6f9d4f5241cafad00497970bb78
DOUGLAS PINTO DO NASCIMENTO <shrek18.5@gmail.com>
2022-04-07 22:18:37 -0300

    Adicionado mais dois exercicios

---
+++ Exercicio_2.py
@@ -0,0 +1,11 @@
+# Escreva um program que pergunte a quantidade de km percorridos por um carro alugado pelo usuário,
+# assim como o quantidade de dias pelos quais o carro foi alugado. Calcule o preço a pagar, sabendo que o carro
+# custa R$ 60,00 por dia e R$ 0,15 por km rodado.
+
+km = int(input('Quantos Km forma percorridos? '))
+dias = int(input('Por quantos dias ele foi alugado? '))
+
+preco = 60 * dias + 0.15 * km
+
+print('Km = {}. Dias {}'.format(km, dias))
+print('Valor a ser pago é de R$ {}'.format(preco))
