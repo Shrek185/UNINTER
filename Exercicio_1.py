5f9686e170c4781583beb117877e8d4cabbcb181
Shrek185 <shrek18.5@gmail.com>
2022-04-07 21:37:26 -0300

    Initial commit

---
+++ Exercicio_1.py
@@ -0,0 +1,3 @@
+# Desenvolva um algoritimo que solicite ao usuário o preço de um produto e um percentual de desconto a ser aplicado a ele.
+# Calcule e exiba o valor do desconto e o preço final do produto (exercício da apostila - aula2)
+
---
+++ Exercicio_Fixacao_2.py
@@ -0,0 +1,19 @@
+# Escreva as expresões em Python para:
+
+# a) Atribuir o valor inteiro 3 à variáviel "a".
+
+a = 3
+
+print(a)
+
+# b) Atribuir o valor inteiro 4 à variáviel "b".
+
+b = 4
+
+print(b)
+
+# c) Atribuir à variável "c" o valor da expressão "a*a+b*b".
+
+c = ((a*a)+(b*b))
+
+print(c)
