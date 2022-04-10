b46c82facf29bad1865677ac53fd61b090a94ccf
Shrek185 <shrek18.5@gmail.com>
2022-04-10 16:10:09 -0300

    Calculo de consumo de energia

--- Exercicio_7.py
+++ Exercicio_7.py
@@ -9,4 +9,46 @@ R para residências;
 I para indústria
 C para comércio
 
+  *********************************************
+  * Preço por tipo e faixa de consumo         *
+  * *******************************************
+  * Tipo        * Faixa (kWh)    * Preço (R$) *
+  *********************************************
+  *             * Até 500        * 0,40       *
+  * Residencial *******************************
+  *             * Acima de 500   * 0,65       *
+  *********************************************
+  *             * Até 1000       * 0,55       *
+  * Comercial   *******************************
+  *             * Acima 1000     * 0,60       *
+  *********************************************
+  *             *  Até 5000      * 0,55       *
+  * Industrial  *******************************
+  *             * Acima de 5000  *  0,60      *
+  *********************************************
+
 """
+kWh = float(input('Quanto kWh?'))
+tipo = input('Qual o tipo de instalação (R, C ou I)')
+
+if (tipo == "R"):
+    if kWh <= 500:
+        preco = 0.4
+    else:
+        preco = 0.65
+
+elif (tipo == 'C'):
+    if kWh <= 1000:
+        preco = 0.55
+    else:
+        preco = 0.60
+
+elif (tipo == 'I'):
+    if kWh <= 5000:
+        preco = 0.55
+    else:
+        preco = 0.60
+else:
+    print('Instalação inválida!')
+
+print('Total a pagar: R$ {}'.format(preco * kWh))
