cae61b3582957cd944f4422cd7b029110ec838f1
Shrek185 <shrek18.5@gmail.com>
2022-04-10 09:47:02 -0300

    Implementação do exercício prático 7

---
+++ Exercicio_Fixacao_7.py
@@ -0,0 +1,22 @@
+"""
+Autor: Douglas Nascimento (Shrek18.5)
+
+Traduza as afirmações a seguir para condicionais em Python:
+
+a) Se ano é divisível por 4, escreva: "Pode ser um ano bissexto".
+    Caso contrário, escreva: "Definitivamente não é ium ano bissexto"
+
+**********************************************************************************************************
+* Bissexto - Um ano é bissexto se ele for divisível por 400 ou se ele for divisível por 4 e não por 100. *
+**********************************************************************************************************
+
+b) Se ambas as variáveios booleanas cima e baixo forem True, escreva: "Decida-se",
+    caso contrário, escreva: "Você escolheu um caminho!"
+
+"""
+
+ano = int(input('Digite o ano: '))
+if ano % 100 != 0 and ano % 4 == 0 or ano % 400 == 0:
+    print('É um ano bissexto')
+else:
+    print('Não é bissexto')
