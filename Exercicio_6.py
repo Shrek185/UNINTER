bf6f03c1cb6e0077c9fced345b5b5330d2a824a3
Shrek185 <shrek18.5@gmail.com>
2022-04-10 11:01:29 -0300

    Calculadora

---
+++ Exercicio_6.py
@@ -0,0 +1,53 @@
+"""
+Autor : Douglas Nascimento (Shrek18.5)
+Obs: Execício baseado no curso de ADS da UNINTER modulo EAD de 2022
+
+Escreva um Algoritmo que leia dois valores numéricos e que pergunte ao usuário qual operação ele deseja realizar:
+Adição (+);
+Subtração (-);
+Multiplicação (*);
+Divisão (/).
+Exiba na tela o resultado da operação desejada.
+"""
+
+print('Calculadora ADS 2022')
+print('+ Adição')
+print('- Subtração')
+print('* Multiplicação')
+print('/ Divisão')
+print('Pressione outra tecla para sair')
+
+op = input('Qual operação deseja fazer?')
+if op == '+' or op == '-' or op == '*' or op == '/':
+
+    x = int(input('Digite o primeiro valor: '))
+    y = int(input('Digite o segundo valor: '))
+
+if (op == '+'):
+
+    res = x + y
+
+    print('Resultado: {} + {} = {}'.format(x, y, res))
+
+elif (op == '-'):
+
+    res = x - y
+
+    print('Resultado: {} - {} = {}'.format(x, y, res))
+
+elif (op == '*'):
+
+    res = x * y
+
+    print('Resultado: {} * {} = {}'.format(x, y, res))
+
+elif (op == '/'):
+
+    res = x / y
+
+    print('Resultado: {} / {} = {}'.format(x, y, res))
+
+else:
+    print("Operação inválida.")
+
+print('Encerramendo o programa...')
