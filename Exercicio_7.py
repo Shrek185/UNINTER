f0547a64a1abbf9a2a98097826b533f11c588fa4
Shrek185 <shrek18.5@gmail.com>
2022-04-10 13:37:05 -0300

    Calculo de Consumo de energia

---
+++ Exercicio_7.py
@@ -0,0 +1,12 @@
+"""
+Autor : Douglas Nascimento (Shrek18.5)
+Obs: Execício baseado no curso de ADS da UNINTER modulo EAD de 2022
+
+Escreva um programa que calcule o preço a pagar pelo fornecimento de energia elétrica.
+Pergunte a quantidade de kWh consumida e o tipo de instalação:
+
+R para residências;
+I para indústria
+C para comércio
+
+"""
