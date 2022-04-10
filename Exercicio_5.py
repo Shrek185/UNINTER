e346e9b14f904abdcb74e7587daa1ed1c9476263
Shrek185 <shrek18.5@gmail.com>
2022-04-10 10:15:39 -0300

    Exercícios dos triângulos

--- Exercicio_5.py
+++ Exercicio_5.py
@@ -9,5 +9,27 @@ a) Equilátero (três lados iguais)
 b) Isósceles (dis lados iguais)
 c) Escaleno (três lados diferentes)
 
-Autor : Douglas Nascimento (Shrek18.5)
+Lembre-se de que, para forma um triângulo, nenhum dos lados pode ser igual a zero, e um lado não pode ser
+maior que a soma dos outros dois.
+
 """
+A = int(input('Digite o primeiro lado do triângulo: '))
+B = int(input('Digite o segundo lado do triângulo: '))
+C = int(input('Digite o terceiro lado do triângulo: '))
+
+if (A > 0) and (B > 0) and (C > 0):
+    if (A + B > C) and (A + C > B) and (B + C > A):
+
+    # Se você chegou até aqui, é porque o triângulo é válido.
+        if (A != B) and (A != C) and (B != C):
+            print('Triângulo escaleno!')
+        else:
+            if A == B and A == C and B == C:
+                print("Triângulo equilátero!")
+            else:
+                print('Triângulo isósceles!')
+    else:
+        print("Ao menos um dos valores indicado não servem para formar um triângulo")
+
+else:
+    print("Ao menos um dos valores indicado não servem para formar um triângulo")
